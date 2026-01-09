d41a8663c86ce5b37b380a8be64b69a59d5129e2
Ian Dorion <madeindreams@protonmail.com>
2026-01-09 15:51:42 -0500

    lock file

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,11 @@ export default defineConfig({
     test: {
         environment: "happy-dom",
         include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "lcov"],
+            reportsDirectory: "./coverage",
+            exclude: ["src/react/**"]
+        },
     },
 });
